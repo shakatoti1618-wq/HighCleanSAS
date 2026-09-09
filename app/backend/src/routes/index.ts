979e9f1b7dc7c4d641bb745a8e23b72d59e48ff0
Jonathan Heredia <shakatoti1618@gmail.com>
2026-09-08 21:49:34 -0500

    feat: register company route

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
+import companyRouter from './company.routes.js'
 import healthRouter from './health.routes.js'
 
 const apiRouter = Router()
 
 apiRouter.use('/health', healthRouter)
+apiRouter.use('/company', companyRouter)
 
 export default apiRouter
