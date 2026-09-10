dbce2a3aade05ecf1bb5e2b7bc3c020836d21000
Jonathan Heredia <shakatoti1618@gmail.com>
2026-09-09 19:59:11 -0500

    feat: register service route

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import companyRouter from './company.routes.js'
 import healthRouter from './health.routes.js'
+import serviceRouter from './service.routes.js'
 
 const apiRouter = Router()
 
 apiRouter.use('/health', healthRouter)
 apiRouter.use('/company', companyRouter)
+apiRouter.use('/services', serviceRouter)
 
 export default apiRouter
