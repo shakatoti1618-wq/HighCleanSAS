23c2c0d1e0ae61518592110ad2ad55f914e289ce
Jonathan Heredia <shakatoti1618@gmail.com>
2026-09-10 18:44:31 -0500

    feat: register gallery route

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import companyRouter from './company.routes.js'
+import galleryRouter from './gallery.routes.js'
 import healthRouter from './health.routes.js'
 import serviceRouter from './service.routes.js'
 
@@ -8,5 +9,6 @@ const apiRouter = Router()
 apiRouter.use('/health', healthRouter)
 apiRouter.use('/company', companyRouter)
 apiRouter.use('/services', serviceRouter)
+apiRouter.use('/gallery', galleryRouter)
 
 export default apiRouter
