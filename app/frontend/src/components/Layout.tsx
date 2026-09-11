58a8c5e4e373d6bb2de0215344991fcedea76bd0
Jonathan Heredia <shakatoti1618@gmail.com>
2026-09-10 23:09:40 -0500

    feat: enable motion config and skip link in layout

--- app/frontend/src/components/Layout.tsx
+++ app/frontend/src/components/Layout.tsx
@@ -1,16 +1,27 @@
 import { Outlet } from 'react-router-dom'
+import { MotionConfig } from 'motion/react'
+import Background from './Background.tsx'
 import Footer from './Footer.tsx'
 import Navbar from './Navbar.tsx'
 
 function Layout() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Navbar />
-      <main className="flex-1">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
+    <MotionConfig reducedMotion="user">
+      <div className="relative flex min-h-screen flex-col">
+        <a
+          href="#main"
+          className="sr-only focus:not-sr-only focus:absolute focus:left-4 focus:top-4 focus:z-50 focus:rounded-sm focus:bg-brand-turq focus:px-4 focus:py-2 focus:text-white"
+        >
+          Saltar al contenido
+        </a>
+        <Background />
+        <Navbar />
+        <main id="main" className="relative z-10 flex-1">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </MotionConfig>
   )
 }
 
