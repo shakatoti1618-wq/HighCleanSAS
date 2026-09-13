eb5f7135a03c16ba06a832662ba4807a85713ccb
Jonathan Heredia <shakatoti1618@gmail.com>
2026-09-13 13:31:23 -0500

    feat: register review route

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import companyRouter from './company.routes.js'
 import galleryRouter from './gallery.routes.js'
 import healthRouter from './health.routes.js'
+import reviewRouter from './review.routes.js'
 import serviceRouter from './service.routes.js'
 
 const apiRouter = Router()
@@ -10,5 +11,6 @@ apiRouter.use('/health', healthRouter)
 apiRouter.use('/company', companyRouter)
 apiRouter.use('/services', serviceRouter)
 apiRouter.use('/gallery', galleryRouter)
+apiRouter.use('/reviews', reviewRouter)
 
 export default apiRouter
