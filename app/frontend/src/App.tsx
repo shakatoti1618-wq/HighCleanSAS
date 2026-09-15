a42e82a74f043d401e6f523608c2c4a7bcd7046c
Jonathan Heredia <shakatoti1618@gmail.com>
2026-09-14 19:11:50 -0500

    feat: register login and admin routes

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout.tsx'
+import ProtectedRoute from './components/ProtectedRoute.tsx'
+import Admin from './pages/Admin.tsx'
 import Contacto from './pages/Contacto.tsx'
 import Galeria from './pages/Galeria.tsx'
 import Home from './pages/Home.tsx'
+import Login from './pages/Login.tsx'
 import Nosotros from './pages/Nosotros.tsx'
 import Servicios from './pages/Servicios.tsx'
 
@@ -12,6 +15,7 @@ const Resenas = lazy(() => import('./pages/Resenas.tsx'))
 function App() {
   return (
     <Routes>
+      <Route path="login" element={<Login />} />
       <Route element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="servicios" element={<Servicios />} />
@@ -26,6 +30,12 @@ function App() {
           }
         />
         <Route path="contacto" element={<Contacto />} />
+        <Route
+          path="admin"
+          element={
+            <ProtectedRoute>{(user) => <Admin user={user} />}</ProtectedRoute>
+          }
+        />
       </Route>
     </Routes>
   )
