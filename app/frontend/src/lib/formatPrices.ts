003a9903c6d424075ab5e238b382ca6a15cb8d69
Jonathan Heredia <shakatoti1618@gmail.com>
2026-09-24 00:15:39 -0500

    feat: format Colombian peso prices

---
+++ app/frontend/src/lib/formatPrices.ts
@@ -0,0 +1,9 @@
+const copFormatter = new Intl.NumberFormat('es-CO', {
+  style: 'currency',
+  currency: 'COP',
+  maximumFractionDigits: 0,
+})
+
+export function formatCOP(price: number): string {
+  return copFormatter.format(price)
+}
